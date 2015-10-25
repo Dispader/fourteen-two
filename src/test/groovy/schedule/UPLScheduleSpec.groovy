14bff2791fa3585e8bbe4899fb96762de2651b9c
Jake Gage <jake.gage@gmail.com>
2015-10-25 15:28:13 -0700

    adds first Spock specification test

---
+++ src/test/groovy/schedule/UPLScheduleSpec.groovy
@@ -0,0 +1,13 @@
+package schedule
+
+import spock.lang.Specification
+
+class UPLScheduleSpec extends Specification {
+
+    def 'the first test runs'() {
+        when: 'we we do stuff'
+        then: 'stuff happens'
+            true
+    }
+
+}
