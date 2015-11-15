2ede5160b1b29da93c78c26d631ca2ed09bbea71
Jake Gage <jake.gage@gmail.com>
2015-11-15 16:46:53 -0600

    moves test file into proper resources directory location

--- src/test/groovy/org/domuique/fourteentwo/TeamResourceSpec.groovy
+++ src/test/groovy/org/domuique/fourteentwo/TeamResourceSpec.groovy
@@ -12,14 +12,14 @@ class TeamResourceSpec extends Specification {
 
     def 'unit tests can load a Fall 2015 Advanced Sunday schedule test file'() {
         when:
-            String contents = this.getClass().getResource('/2015-Fall-advsundiv.txt').text
+            String contents = this.getClass().getResource('/2015/fall/advsundiv.txt').text
         then:
             contents
     }
 
     def 'the team resource can process a Fall 2015 Advanced Sunday schedule test file'() {
         when:
-            File file = this.getResourceAsFile('/2015-Fall-advsundiv.txt')
+            File file = this.getResourceAsFile('/2015/fall/advsundiv.txt')
             Collection<Team> teams = TeamResource.get(file)
         then:
             teams?.size() == 36
