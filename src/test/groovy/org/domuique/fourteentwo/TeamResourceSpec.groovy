fe800844cb19fd44a23c8d8ae1080303dbfffee6
Jake Gage <jake.gage@gmail.com>
2015-11-15 15:56:52 -0600

    extracts method for extracting teams by line

--- src/test/groovy/org/domuique/fourteentwo/TeamResourceSpec.groovy
+++ src/test/groovy/org/domuique/fourteentwo/TeamResourceSpec.groovy
@@ -25,4 +25,16 @@ class TeamResourceSpec extends Specification {
             teams?.size() == 36
     }
 
+    def 'the team resource can extract teams from lines of a schedule file'() {
+        when:
+            Team extractedTeam = TeamResource.extract(line)
+        then:
+            extractedTeam == new Team(id: teamId, name: teamName)
+        where:
+            line                                      | teamId  | teamName
+            '72103 14 Balls & a Rack 412 3438 28 123' | '72103' | '14 Balls & a Rack'
+            '72105 Hell\'s Face 441 3619 30 121'      | '72105' | 'Hell\'s Face'
+            //'72307 Stoopid 2 937 4114 27 152'         | '72307' | 'Stoopid 2' // fails with final #
+    }
+
 }
