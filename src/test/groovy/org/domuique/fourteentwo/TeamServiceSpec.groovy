e1031f92baec9ed01575b3396d17f2b78d9d526c
Jake Gage <jake.gage@gmail.com>
2015-11-15 01:00:16 -0600

    adds workaround solving space enconding during test execution
    When CI tests are wrapped in scripts for execution in certain environments,
    spaces obtained as JVM resources (useful for safe relative-path resolution)
    are encoded as URLs.  While useful and largely portable, when executions
    are wrapped in scripts, shell-executing environments fail to read these
    when searching for local paths.  This causes test resources to not be found.
    
    See http://jlorenzen.blogspot.com/2007/06/proper-way-to-access-file-resources-in.html
    (I8UMuch comments, on September 11th, 2009)
    
    This hack obtains the URL in universal format, and manually re-writes the
    String output to re-replace the escaped spaces.  IMHO, this will probably
    cause problems for other encoded.  A better solution would probably be
    to decode the URL entirely to String format to feed to the file.
    
    I've added a TODO to create a re-usable getResourceAsFile() method.

--- src/test/groovy/org/domuique/fourteentwo/TeamServiceSpec.groovy
+++ src/test/groovy/org/domuique/fourteentwo/TeamServiceSpec.groovy
@@ -13,8 +13,11 @@ class TeamServiceSpec extends Specification {
 
     def 'the team factory class can process a Fall 2015 Advanced Sunday schedule test file'() {
         when:
-            File scheduleFile = new File(this.getClass().getResource('/2015-Fall-advsundiv.txt').getFile())
-            Collection<Team> teams = TeamFactory.get(scheduleFile)
+            // TODO: REFACTOR this to a getResourceAsFile method for re-use
+            URL url = this.getClass().getResource('/2015-Fall-advsundiv.txt')
+            String filepath = url.getFile().replace('%20', ' ')
+            File file = new File(filepath)
+            Collection<Team> teams = TeamFactory.get(file)
         then:
             teams?.size() == 36
     }
