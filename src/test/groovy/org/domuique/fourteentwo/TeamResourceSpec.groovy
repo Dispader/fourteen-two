23ae85819238e283df16ab75cbe879ede46f12de
Jake Gage <jake.gage@gmail.com>
2015-11-15 15:56:18 -0600

    extracts method for obtaining files form resources

--- src/test/groovy/org/domuique/fourteentwo/TeamResourceSpec.groovy
+++ src/test/groovy/org/domuique/fourteentwo/TeamResourceSpec.groovy
@@ -2,7 +2,13 @@ package org.domuique.fourteentwo
 
 import spock.lang.Specification
 
-class TeamServiceSpec extends Specification {
+class TeamResourceSpec extends Specification {
+
+    private File getResourceAsFile(String resource) {
+        URL url = this.getClass().getResource(resource)
+        String filepath = url.getFile().replace('%20', ' ')
+        new File(filepath)
+    }
 
     def 'unit tests can load a Fall 2015 Advanced Sunday schedule test file'() {
         when:
@@ -11,13 +17,10 @@ class TeamServiceSpec extends Specification {
             contents
     }
 
-    def 'the team factory class can process a Fall 2015 Advanced Sunday schedule test file'() {
+    def 'the team resource can process a Fall 2015 Advanced Sunday schedule test file'() {
         when:
-            // TODO: REFACTOR this to a getResourceAsFile method for re-use
-            URL url = this.getClass().getResource('/2015-Fall-advsundiv.txt')
-            String filepath = url.getFile().replace('%20', ' ')
-            File file = new File(filepath)
-            Collection<Team> teams = TeamFactory.get(file)
+            File file = this.getResourceAsFile('/2015-Fall-advsundiv.txt')
+            Collection<Team> teams = TeamResource.get(file)
         then:
             teams?.size() == 36
     }
