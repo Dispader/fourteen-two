2eee07f79dd699c4cb47bd9ff13d26ef2a596d26
Jake Gage <jake.gage@gmail.com>
2015-11-22 23:30:50 -0600

    adds method to extract all teams from a schedule

--- src/main/groovy/org/domuique/fourteentwo/ScheduleResource.groovy
+++ src/main/groovy/org/domuique/fourteentwo/ScheduleResource.groovy
@@ -6,12 +6,23 @@ class ScheduleResource {
         UPL.getText 'http://www.m8pool.com/pdfs/advsunsched.pdf'
     }
 
-    private static Map extractTeam(String line, String teamName = '14 Balls & a Rack') {
+    private static Map extractTeamFromLine(String line, String teamName = '14 Balls & a Rack') {
         def expression = "^(\\d{1,2}?)\\s+${ -> teamName}\\s+(.*?)\\s\\(.*\$"
         def team = (line=~expression).collect { match, listing, home ->
            [ 'listing': listing as Integer, 'name':teamName, 'home': home ]
         }
         team.listing ? team.first() : null
+    }
+
+    private static Map<Integer, Map> extractTeams(String standings, List<String> teamNames) {
+        def map = [:]
+        standings.eachLine { standingsLine ->
+            teamNames.each { teamName ->
+                def team = ScheduleResource.extractTeamFromLine(standingsLine, teamName)
+                if ( team ) { map << [ (team.listing): team ] }
+            }
+        }
+        map
     }
 
     public static String getTeams() {
--- src/test/groovy/org/domuique/fourteentwo/ScheduleResourceSpec.groovy
+++ src/test/groovy/org/domuique/fourteentwo/ScheduleResourceSpec.groovy
@@ -4,9 +4,9 @@ import spock.lang.Specification
 
 class ScheduleResourceSpec extends Specification {
 
-    def 'we can extract teams from a line of a schedule file'() {
+    def 'we can extract a team from a line of a schedule'() {
         given:
-            def team = ScheduleResource.extractTeam(line, name)
+            def team = ScheduleResource.extractTeamFromLine(line, name)
         expect:
             team['listing'] == listing
             team['name'] == name
@@ -22,6 +22,45 @@ class ScheduleResourceSpec extends Specification {
             "PBA"               | "7 PBA Shooter's Billiards (952)894-1100 1934 Highway 13 East Burnsville MN"              | 7        | "Shooter's Billiards"
             "Brooklyn Saints"   | "8 Brooklyn Saints Shooter's Billiards (952)894-1100 1934 Highway 13 East Burnsville MN"  | 8        | "Shooter's Billiards"
             "Unleash the Fury"  | "9 Unleash the Fury Shooter's Billiards (952)894-1100 1934 Highway 13 East Burnsville MN" | 9        | "Shooter's Billiards"
+    }
+
+    def 'we can extract a list of teams from a schedule'() {
+        given:
+            def schedule = """
+1 Tire Checkers Hexagon Bar (612)722-3454 2600 27th Av. S Minneapolis MN
+2 We're Hexed Hexagon Bar (612)722-3454 2600 27th Av. S Minneapolis MN
+3 14 Balls & a Rack Cardinal Tavern (612)724-5837 2920 E 38th St. Minneapolis MN
+4 Da Ezoob Code Cardinal Tavern (612)724-5837 2920 E 38th St. Minneapolis MN
+5 Hell's Face Station 280 (651)233-2165 2554 Como Av. St. Paul MN
+6 Shoot 'em Dead Tavern on Avenue (651)227-6315 825 Jefferson Av. St. Paul MN
+7 PBA Shooter's Billiards (952)894-1100 1934 Highway 13 East Burnsville MN
+8 Brooklyn Saints Shooter's Billiards (952)894-1100 1934 Highway 13 East Burnsville MN
+9 Unleash the Fury Shooter's Billiards (952)894-1100 1934 Highway 13 East Burnsville MN"""
+            def names = 
+[ "Tire Checkers", 
+  "We're Hexed",
+  "14 Balls & a Rack",
+  "Da Ezoob Code",
+  "Hell's Face",
+  "Shoot 'em Dead",
+  "PBA",
+  "Brooklyn Saints",
+  "Unleash the Fury", ]
+        when:
+            def teams = ScheduleResource.extractTeams(schedule, names)
+        then:
+            teams[listing] == [ 'listing': listing, 'name': name, 'home': home ]
+        where:
+            listing  | name                | home
+            1        | "Tire Checkers"     | "Hexagon Bar"
+            2        | "We're Hexed"       | "Hexagon Bar"
+            3        | "14 Balls & a Rack" | "Cardinal Tavern"
+            4        | "Da Ezoob Code"     | "Cardinal Tavern"
+            5        | "Hell's Face"       | "Station 280"
+            6        | "Shoot 'em Dead"    | "Tavern on Avenue"
+            7        | "PBA"               | "Shooter's Billiards"
+            8        | "Brooklyn Saints"   | "Shooter's Billiards"
+            9        | "Unleash the Fury"  | "Shooter's Billiards"
     }
 
 }
